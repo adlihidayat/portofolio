1acb79a28a23ec25067d07e68d225b1b11de9dce
oneiric 30 <adlihidayat30@gmail.com>
2023-06-22 15:08:50 +0700

    lazy to eager

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,4 +1,4 @@
-import img from "next/image";
+import Image from "next/image";
 import React, { useState } from "react";
 
 function Projects() {
@@ -36,13 +36,19 @@ function Projects() {
           onClick={() => setDetails(1)}
           className="relative animate-project1-mobile xl:animate-project1-dekstop flex-1 md:hover:flex-none md:hover:w-[450px] lg:hover:w-[500px] xl:hover:w-[600px] 2xl:hover:w-[700px] duration-500 ease-in-out h-80 sm:h-[500px] xl:h-[600px] 2xl:h-[700px] bg-slate-200"
         >
-          <img src="/nameasy.png" className=" h-[100%] object-cover" alt="" />
+          <img
+            loading="eager"
+            src="/nameasy.png"
+            className=" h-[100%] object-cover"
+            alt=""
+          />
         </div>
         <div
           onClick={() => setDetails(2)}
           className="relative animate-project2-mobile xl:animate-project2-dekstop flex-1 md:hover:flex-none md:hover:w-[450px] lg:hover:w-[500px] xl:hover:w-[600px] 2xl:hover:w-[700px] duration-500 ease-in-out h-80 sm:h-[500px] xl:h-[600px] 2xl:h-[700px] bg-slate-200 mt-10"
         >
           <img
+            loading="eager"
             src="/glassmorphism.png"
             className=" h-[100%] object-cover"
             alt=""
@@ -52,13 +58,23 @@ function Projects() {
           onClick={() => setDetails(3)}
           className="relative animate-project1-mobile xl:animate-project1-dekstop flex-1 md:hover:flex-none md:hover:w-[450px] lg:hover:w-[500px] xl:hover:w-[600px] 2xl:hover:w-[700px] duration-500 ease-in-out h-80 sm:h-[500px] xl:h-[600px] 2xl:h-[700px] bg-slate-200"
         >
-          <img src="/salfiyah.png" className=" h-[100%] object-cover" alt="" />
+          <img
+            loading="eager"
+            src="/salfiyah.png"
+            className=" h-[100%] object-cover"
+            alt=""
+          />
         </div>
         <div
           onClick={() => setDetails(4)}
           className=" relative animate-project2-mobile xl:animate-project2-dekstop flex-1 md:hover:flex-none md:hover:w-[450px] lg:hover:w-[500px] xl:hover:w-[600px] 2xl:hover:w-[700px] duration-500 ease-in-out h-80 sm:h-[500px] xl:h-[600px] 2xl:h-[700px] bg-slate-200 mt-10"
         >
-          <img src="/tictactoe.png" className=" h-[100%] object-cover" alt="" />
+          <img
+            loading="eager"
+            src="/tictactoe.png"
+            className=" h-[100%] object-cover"
+            alt=""
+          />
         </div>
       </div>
       {details !== 0 && (
